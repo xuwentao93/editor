dddab8de1ecca4f99b0c831ee8efbb6cd0072bce
xuwentao93 <a15157756529@gmail.com>
2020-01-02 00:14:12 +0800

    more key events.

--- src/components/write/index.jsx
+++ src/components/write/index.jsx
@@ -3,6 +3,12 @@ import './index.css'
 
 let stop = false // 中文输入的情况下让文本框的内容正确外弹. 放在函数内部每次渲染的时候都会重置
 // 为false, 用于 state 的时候会有延迟性.
+let timer = null // 光标显影的定时器.
+let staticX = 0
+let prevText = ''
+let endText = ''
+let staticLeft = 0
+let N = 0
 
 export default function Write() { // conclusion: keyEvent 事件一定先于 onCange 事件.
   const [showCursor, setShowCursor] = useState(false) // 光标的显隐.
@@ -23,7 +29,7 @@ export default function Write() { // conclusion: keyEvent 事件一定先于 onC
       getLength.current.innerHTML = ''
       return parseFloat(width)
     },
-    caretLeft(x, ifAdd = true) { // 设置光标左右的位置.
+    caretHorizontalMove(x = X, ifAdd = true) { // 设置光标左右的位置.
       const width = utils.getStringLength(textList[line][x - 1])
       const left = window.getComputedStyle(caret.current).left
       if (ifAdd) caret.current.style.left = parseFloat(left) + width + 'px'
@@ -36,7 +42,8 @@ export default function Write() { // conclusion: keyEvent 事件一定先于 onC
       if (showCursorController) {
         setShowCursor(true)
         setShowCursorController(false)
-        setCursorInterval(setInterval(() => setShowCursor(prevCursor => !prevCursor), 500))
+        timer = setInterval(() => setShowCursor(prevCursor => !prevCursor), 500)
+        setCursorInterval(timer)
         // setShowCursor 必须写成函数形式, 否则无法实时更新!
         textarea.current.focus()
       }
@@ -47,24 +54,42 @@ export default function Write() { // conclusion: keyEvent 事件一定先于 onC
       clearInterval(cursorInterval)
     },
     getText() {
-      // if (stop) return
       const value = textarea.current.value
+      if (stop) {
+        if (N === 0) {
+          N = 1
+          prevText = textList[line].slice(0, X)
+          endText = textList[line].slice(X)
+          staticX = X
+        }
+        const newText = Object.assign([], textList)
+        newText[line] = prevText + value + endText
+        setTextList(newText)
+        setX(staticX + value.length)
+        // utils.caretHorizontalMove(X + 1)
+        return
+      }
       const left = window.getComputedStyle(caret.current).left
       const width = utils.getStringLength(value)
       caret.current.style.left = parseFloat(left) + width + 'px'
       if (value !== '\n') {
         const newText = Object.assign([], textList)
-        newText[line] += value
+        newText[line] = newText[line].slice(0, X) + value + newText[line].slice(X)
         setX(X + value.length)
         // 上述代码可以直接替换不用 setTextList, 不过更新起来会非常慢.
         setTextList(newText)
       }
       textarea.current.value = ''
+      N = 0
     },
     keyEvent(event) {
       if (stop) return
+      clearInterval(timer)
+      setShowCursor(true)
+      timer = setInterval(() => setShowCursor(prevCursor => !prevCursor), 500)
       const key = {
         Backspace() {
+          // console.log(X)
           if (X === 0) {
             if (line === 0) return
             const style = caret.current.style
@@ -77,7 +102,7 @@ export default function Write() { // conclusion: keyEvent 事件一定先于 onC
             return
           }
           const newText = Object.assign([], textList)
-          utils.caretLeft(X, false)
+          utils.caretHorizontalMove(X, false)
           newText[line] = newText[line].slice(0, newText[line].length - 1)
           // console.log(newText)
           setTextList(newText)
@@ -94,28 +119,64 @@ export default function Write() { // conclusion: keyEvent 事件一定先于 onC
         },
         ArrowLeft() {
           console.log(X)
-          if (X === 1) return
-          utils.caretLeft(X, false)
+          if (X === 0) return
+          utils.caretHorizontalMove(X, false)
           setX(X - 1)
         },
         ArrowRight() {
-          console.log(X)
           if (X === textList[line].length) return
-          utils.caretLeft(X)
+          utils.caretHorizontalMove(X + 1)
           setX(X + 1)
         },
-        ArrowTop() {
-
+        ArrowUp() {
+          const style = caret.current.style
+          if (line === 0) {
+            style.left = '0'
+            setX(0)
+          } else if 
+          (utils.getStringLength(textList[line].slice(0, X))
+          >=
+          utils.getStringLength(textList[line - 1])
+          || textList[line].slice(0, X).length >= textList[line - 1].length) {
+            style.top = parseFloat(window.getComputedStyle(caret.current).top) - 20.8 + 'px'
+            style.left = utils.getStringLength(textList[line - 1]) + 'px'
+            setX(textList[line - 1].length)
+            setLine(line - 1)
+          } else {
+            style.top = parseFloat(window.getComputedStyle(caret.current).top) - 20.8 + 'px'
+            style.left = utils.getStringLength(textList[line - 1].slice(0, X)) + 'px'
+            setLine(line - 1)
+          }
         },
         ArrowDown() {
-          
+          console.log(line)
+          console.log(textList.length - 1)
+          const style = caret.current.style
+          if (line === textList.length - 1) {
+            style.left = utils.getStringLength(textList[line]) + 'px'
+            setX(textList[line].length)
+          } else if 
+          (utils.getStringLength(textList[line].slice(0, X))
+          >=
+          utils.getStringLength(textList[line + 1])
+          || textList[line].slice(0, X).length >= textList[line + 1].length) {
+            style.top = parseFloat(window.getComputedStyle(caret.current).top) + 20.8 + 'px'
+            style.left = utils.getStringLength(textList[line + 1]) + 'px'
+            setX(textList[line + 1].length)
+            setLine(line + 1)
+          } else {
+            style.top = parseFloat(window.getComputedStyle(caret.current).top) + 20.8 + 'px'
+            style.left = utils.getStringLength(textList[line + 1].slice(0, X)) + 'px'
+            setLine(line + 1)
+          }
         }
       }
       if (key[event.key]) key[event.key]()
     },
     onCompositionEnd() { // 中文输入法结束后
       stop = false
-      methods.getText()
+      textarea.current.value = ''
+      // methods.getText()
     }
   }
   return (
