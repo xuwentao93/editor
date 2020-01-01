0418e4a47dde34d45d350f9175ad06712622f24c
xuwentao93 <a15157756529@gmail.com>
2020-01-01 22:15:11 +0800

    add lots of things, move caret, some key events and so on

--- README.md
+++ README.md
@@ -1,68 +1,11 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1. 光标的主动移动. 键盘事件上下左右可以移动光标, 然后从光标的位置输入文本, 继而修改当前列. 点击页面后移动光标至相应的位置.
 
-## Available Scripts
+2. 文本的修改. 在文本域中输入, 获取其 value 值在将里面的value导出到页面, 最后清空.
 
-In the project directory, you can run:
+3. 光标的被动移动以及文本溢出. 在输入文字后光标后移, 当当前行溢出, 光标移至下一行. 若下一行也溢出, 则将文末字符在移至下一行, 不断循环为止. 当碰见空的一行, 则创建新的一行, 将之前最后一行的文本的溢出内容移至新的一行. (难点: 字母,数字, 中文标点符号的长度不等.)
 
-### `yarn start`
+4. 编辑事件: Fn + ←, ctrl + ←, Fn + shift + ← 等等编辑事件的应用.
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+5. 功能: 粘贴, 拖拽, 加粗, 引入图片, 引入链接.
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+6. 拖蓝. 拖蓝的时候要使输入框失焦, 拖拽后能粘贴到对应的地方.
--- src/components/write/index.jsx
+++ src/components/write/index.jsx
@@ -1,21 +1,43 @@
 import React, { useState, useRef } from 'react'
 import './index.css'
 
-export default function Write() {
+let stop = false // 中文输入的情况下让文本框的内容正确外弹. 放在函数内部每次渲染的时候都会重置
+// 为false, 用于 state 的时候会有延迟性.
+
+export default function Write() { // conclusion: keyEvent 事件一定先于 onCange 事件.
   const [showCursor, setShowCursor] = useState(false) // 光标的显隐.
   const [showCursorController, setShowCursorController] = useState(true)
   const [cursorInterval, setCursorInterval] = useState(null)
-  // const [textareaValue, setTextareaValue] = useState('')
-  const [text, setText] = useState('')
-  // const [ifChangeText, setIfChangeText] = useState(true)
-  // 因为 onChange 方法会执行两次, 所以设置一个控制器.
-  const textarea = useRef()
+  const [textList, setTextList] = useState(['']) // 储存文本内容的数组.
+  const [line, setLine] = useState(0) // 光标的行数
+  const [X, setX] = useState(0) // 光标, 输入框位置
+
+  const textarea = useRef() // texaarea
+  const caret = useRef() // textarea 和 caret 的父节点.
+  const getLength = useRef() // 输入删除文本的长度的一个节点.
+
+  const utils = {
+    getStringLength(str) { // 获取文本在实际页面中的长度.
+      getLength.current.innerHTML = str
+      const width = window.getComputedStyle(getLength.current).width
+      getLength.current.innerHTML = ''
+      return parseFloat(width)
+    },
+    caretLeft(x, ifAdd = true) { // 设置光标左右的位置.
+      const width = utils.getStringLength(textList[line][x - 1])
+      const left = window.getComputedStyle(caret.current).left
+      if (ifAdd) caret.current.style.left = parseFloat(left) + width + 'px'
+      else caret.current.style.left = parseFloat(left) - width + 'px'
+    }
+  }
+
   const methods = {
     toggleCursorState() { // 进入页面显示光标
       if (showCursorController) {
         setShowCursor(true)
         setShowCursorController(false)
-        setCursorInterval(setInterval(() => setShowCursor(prevCursor => !prevCursor), 500)) // 必须写成函数形式!
+        setCursorInterval(setInterval(() => setShowCursor(prevCursor => !prevCursor), 500))
+        // setShowCursor 必须写成函数形式, 否则无法实时更新!
         textarea.current.focus()
       }
     },
@@ -24,36 +46,100 @@ export default function Write() {
       setShowCursorController(true)
       clearInterval(cursorInterval)
     },
-    getText(event) { // 为啥 onChange 方法会执行两次?
-      // setIfChangeText(prevIfChangeText => !prevIfChangeText)
-      console.log(1)
-      // if (ifChangeText) {
-      setText(text + event.target.value)
-      // }
+    getText() {
+      // if (stop) return
+      const value = textarea.current.value
+      const left = window.getComputedStyle(caret.current).left
+      const width = utils.getStringLength(value)
+      caret.current.style.left = parseFloat(left) + width + 'px'
+      if (value !== '\n') {
+        const newText = Object.assign([], textList)
+        newText[line] += value
+        setX(X + value.length)
+        // 上述代码可以直接替换不用 setTextList, 不过更新起来会非常慢.
+        setTextList(newText)
+      }
+      textarea.current.value = ''
     },
     keyEvent(event) {
-      if (event.key === 'Backspace') {
-        console.log(text.length)
-        setText(text.slice(0, text.length - 1))
+      if (stop) return
+      const key = {
+        Backspace() {
+          if (X === 0) {
+            if (line === 0) return
+            const style = caret.current.style
+            const width = utils.getStringLength(textList[line - 1])
+            style.top = parseFloat(window.getComputedStyle(caret.current).top) - 20.8 + 'px'
+            style.left = width + 'px'
+            setLine(line - 1)
+            setX(textList[line - 1].length)
+            textList.splice(line, 1)
+            return
+          }
+          const newText = Object.assign([], textList)
+          utils.caretLeft(X, false)
+          newText[line] = newText[line].slice(0, newText[line].length - 1)
+          // console.log(newText)
+          setTextList(newText)
+          setX(X - 1)
+        },
+        Enter() {
+          setLine(line + 1)
+          setX(0)
+          const style = caret.current.style
+          style.left = '0'
+          style.top = parseFloat(window.getComputedStyle(caret.current).top) + 20.8 + 'px'
+          // console.log(style.top)
+          textList.splice(line + 1, 0, '')
+        },
+        ArrowLeft() {
+          console.log(X)
+          if (X === 1) return
+          utils.caretLeft(X, false)
+          setX(X - 1)
+        },
+        ArrowRight() {
+          console.log(X)
+          if (X === textList[line].length) return
+          utils.caretLeft(X)
+          setX(X + 1)
+        },
+        ArrowTop() {
+
+        },
+        ArrowDown() {
+          
+        }
       }
-      // event.stopPropagation()
+      if (key[event.key]) key[event.key]()
+    },
+    onCompositionEnd() { // 中文输入法结束后
+      stop = false
+      methods.getText()
     }
   }
   return (
     <div className="write" onClick={methods.toggleCursorState}>
-      <div className="caret">
+      <div className="caret" ref={caret}>
         <textarea
           className="writing"
           ref={textarea}
           onBlur={methods.leaveWrite}
-          value=""
           onChange={(event) => methods.getText(event)}
+          onCompositionStart={() => { stop = true }}
+          onCompositionEnd={methods.onCompositionEnd}
           onKeyDown={methods.keyEvent} />
         { showCursor ? <div className="cursor" /> : undefined }
       </div>
       <div className="text-list">
-        <div>{text}</div>
+        {
+          textList.map((text, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={index} className="text"><span>{text}</span></div>
+          ))
+        }
       </div>
+      <span className="get-length" ref={getLength}></span>
     </div>
   )
 }
